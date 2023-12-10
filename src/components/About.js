95154e319afeb70e8d2f3edfb22352c842296c7f
joel <joelwilsonjam@gmail.com>
2023-12-09 20:39:58 -0500

    Add github pages settings

--- src/components/About.js
+++ src/components/About.js
@@ -6,7 +6,7 @@ const About = () => {
       <h2 className="text-5xl font-heading  mb-8">
         About me
       </h2>
-      <div className="grid grid-cols-1 md:grid-cols-2 gap-4 md:gap-1">
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-4 md:gap-1 xl:">
         <p className="text-text font-body md:px-4">
           Hello there! I'm Joel Wilson, a passionate and dedicated full stack
           developer with a flair for crafting robust and innovative web
