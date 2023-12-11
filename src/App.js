44d36fe0e584baab1ededab661aaeb64264fa72f
joel <joelwilsonjam@gmail.com>
2023-12-10 23:03:56 -0500

    Added Google analytincs

--- src/App.js
+++ src/App.js
@@ -1,6 +1,13 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import ReactGA from "react-ga";
+
 import HomePage from "./pages/HomePage";
+
 const App = () => {
+  const TRACKING_ID = "UA-253165565-1";
+
+  ReactGA.initialize(TRACKING_ID);
+
   return (
     <>
       <Router>
